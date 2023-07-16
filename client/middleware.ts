d81555ecc8da9d343001afadb5b9aa3994b9817b
PowChorba <101152834+PowChorba@users.noreply.github.com>
2023-07-16 18:31:00 -0300

    css changes on game

---
+++ client/middleware.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest){
+    const requestHeader = new Headers(request.headers)
+    requestHeader.set('SpicyBets', 'Welcome')
+
+    const response = NextResponse.next({
+        request: {
+            headers: requestHeader
+        }
+    })
+
+    if(request.nextUrl.pathname.endsWith('provider/') || request.nextUrl.pathname.endsWith('provider')){
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+}
