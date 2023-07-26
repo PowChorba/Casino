8281a5826f4b7f2e3b25af0f8190905947ee05e1
PowChorba <101152834+PowChorba@users.noreply.github.com>
2023-07-26 10:56:30 -0300

    fix back url

--- client/component/Register/service/register.service.ts
+++ client/component/Register/service/register.service.ts
@@ -3,7 +3,8 @@ import axios from "axios"
 
 export const registerSubmit = async (data: UserRegister) => {
     try {
-        const apiData = await axios.post('http://localhost:3001/' + 'users', data)
+        const apiData = await axios.post(process.env.PETICION_BACK + 'users', data)
+        // const apiData = await axios.post('http://localhost:3001/' + 'users', data)
         return apiData.data
     } catch (error) {
         console.log(error)
